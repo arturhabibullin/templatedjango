f126c21fdb9588c157ada0ddd3738265cee6b1fa
arturhabibullin <artur.habibullin@saria-volga.ru>
2021-09-20 11:14:32 +0300

    base templeate project saria

--- mysite/main/models.py
+++ mysite/main/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Company(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(unique=True)
     
+    def get_absolute_url(self):
+        return reverse('company_detail_url', kwargs={'slug': self.slug})
+
     def __str__(self):
         return self.name
 
@@ -12,12 +16,18 @@ class Product(models.Model):
     name = models.CharField(max_length=50, unique=True) 
     slug = models.SlugField(unique=True)
     
+    def get_absolute_url(self):
+        return reverse('product_detail_url', kwargs={'slug': self.slug})
+
     def __str__(self):
         return self.name
 
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(unique=True)
+
+    def get_absolute_url(self):
+        return reverse('category_detail_url', kwargs={'slug': self.slug})
     
     def __str__(self):
         return self.name
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,10 +1,58 @@
-from main.models import Sample
+from main.models import Category, Company, Product, Sample
 from django.shortcuts import render
 
 # Create your views here.
+def companys_list(request):
+    companys = Company.objects.all()
+    context = {
+        'companys':companys
+    }
+    return render(request, 'main/companys_list.html', context)
+
+def company_detail(request, slug):
+    company = Company.objects.get(slug=slug)
+    sample=company.sample_set.all()
+    context = {
+        'company':company,
+        'sample':sample
+    }
+    return render(request, 'main/company_detail.html', context)
+
+def categorys_list(request):
+    categorys = Category.objects.all()
+    context = {
+        'categorys':categorys
+    }
+    return render(request, 'main/categorys_list.html', context)
+
+
+def category_detail(request, slug):
+    category = Category.objects.get(slug=slug)
+    context = {
+        'category':category
+    }
+    return render(request, 'main/category_detail.html', context)
+
+def products_list(request):
+    products = Product.objects.all()
+    context = {
+        'products':products
+    }
+    return render(request, 'main/products_list.html', context)
+
+def product_detail(request, slug):
+    product = Product.objects.get(slug=slug)
+    sample=product.sample_set.all()
+    context = {
+        'product':product,
+        'sample':sample
+    }
+    return render(request, 'main/product_detail.html', context)
+
 def samples_list(request):
     samples = Sample.objects.all()
     context = {
         'samples':samples
     }
     return render(request, 'main/samples_list.html', context)
+
